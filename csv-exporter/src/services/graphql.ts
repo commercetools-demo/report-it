abd7d45d5092451a1b2ca981591a524907d4f007
Behnam Tehrani <behnam777@gmail.com>
2025-01-23 14:50:48 -0500

    replace variables

--- csv-exporter/src/services/graphql.ts
+++ csv-exporter/src/services/graphql.ts
@@ -1,12 +1,56 @@
 import { createApiRoot } from '../client/create.client';
 
+const DEFAULT_PARAMS = {
+  createdLastWeek: `createdAt <= "${new Date().toISOString()}" and createdAt >= "${new Date(
+    Date.now() - 7 * 24 * 60 * 60 * 1000
+  ).toISOString()}"`,
+  createdLastMonth: `createdAt <= "${new Date().toISOString()}"and createdAt >= "${new Date(
+    Date.now() - 30 * 24 * 60 * 60 * 1000
+  ).toISOString()}"`,
+  createdLastYear: `createdAt <= "${new Date().toISOString()}" and createdAt >= "${new Date(
+    Date.now() - 365 * 24 * 60 * 60 * 1000
+  ).toISOString()}"`,
+};
+
+
+const substituteParams = (
+  variables: Record<string, any>,
+  params: Record<string, any>
+) => {
+  const substituteValue = (value: any): any => {
+    if (typeof value === 'string') {
+      const match = value.match(/:(\w+)/);
+      if (value.startsWith(':') && match?.[1] && params[match[1]]) {
+        return params[match[1]] || value;
+      }
+    } else if (Array.isArray(value)) {
+      return value.map(substituteValue);
+    } else if (typeof value === 'object' && value !== null) {
+      return substituteParams(value, params);
+    }
+    return value;
+  };
+
+  return Object.entries(variables).reduce(
+    (acc: Record<string, any>, [key, value]) => {
+      acc[key] = substituteValue(value);
+      return acc;
+    },
+    {}
+  );
+};
+
 export const gqlFetcher = async ({ query, variables }: any) => {
+  let substitutedVariables;
+  if (variables) {
+    substitutedVariables = substituteParams(variables, DEFAULT_PARAMS);
+  }
   const { body } = await createApiRoot()
     .graphql()
     .post({
       body: {
         query,
-        ...(variables && { variables }),
+        ...(substitutedVariables && { variables: substitutedVariables }),
       },
     })
     .execute();
